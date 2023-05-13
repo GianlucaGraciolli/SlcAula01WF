0689161e886e2428b9b1f62e50cf3caf09ac44ed
Gianluca <gianluca.graciolli@gmail.com>
2023-05-12 22:38:55 -0300

    Commit adicionando formulário principal

---
+++ PrjAula01WF/FormPrincipal.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PrjAula01WF
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            cadastroToolStripMenuItem = new ToolStripMenuItem();
+            veículosToolStripMenuItem = new ToolStripMenuItem();
+            clienteToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, cadastroToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(49, 20);
+            toolStripMenuItem1.Text = "Login";
+            toolStripMenuItem1.Click += toolStripMenuItem1_Click;
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            cadastroToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { veículosToolStripMenuItem, clienteToolStripMenuItem });
+            cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            cadastroToolStripMenuItem.Size = new Size(66, 20);
+            cadastroToolStripMenuItem.Text = "Cadastro";
+            // 
+            // veículosToolStripMenuItem
+            // 
+            veículosToolStripMenuItem.Name = "veículosToolStripMenuItem";
+            veículosToolStripMenuItem.Size = new Size(117, 22);
+            veículosToolStripMenuItem.Text = "Veículos";
+            // 
+            // clienteToolStripMenuItem
+            // 
+            clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            clienteToolStripMenuItem.Size = new Size(117, 22);
+            clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FormPrincipal";
+            Text = "FormPrincipal";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem cadastroToolStripMenuItem;
+        private ToolStripMenuItem veículosToolStripMenuItem;
+        private ToolStripMenuItem clienteToolStripMenuItem;
+    }
+}
---
+++ PrjAula01WF/FormPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrjAula01WF
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //instanciei a classe / criei objeto
+            Form1 janelalogin = new Form1();
+
+            //usando metodo show
+            janelalogin.Show();
+            
+            //definindo a janela "pai" para o form janelaLogin ou seja: obrigando a tela de login criada, ser aberta dentro do formprincipal "login"
+            janelalogin.MdiParent = this;
+        }
+    }
+}
